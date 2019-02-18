cc82283030f115d953cce0af7d00ca2ae0b456b2
st-rom <r.stepaniuk@ucu.edu.ua>
2019-02-18 17:14:42 +0200

    updating

--- src/python/monitors/disk_filesystem_status.py
+++ src/python/monitors/disk_filesystem_status.py
@@ -1,36 +1,38 @@
+import argparse
 import logging
 import subprocess
-import argparse
 import time
-from datetime import datetime
+
+
+error_text = "Didn't match the threshold"
+info_text = "Everything's OK"
 
 
 def main():
-    with open('../../help/disk_filesystem_status_help.txt', 'r') as myfile:
-        descr = myfile.read()
-    parser = argparse.ArgumentParser(description=descr)
+    helper = "Receives the amount of disk space available on the file system containing each file name argument " \
+            "by using bash command du -s [PATH]  along with threshold arguments to compute if file system doesn’t " \
+            "use too much memory space. If no file name is given, the space available on all currently mounted file" \
+            " systems is being processed."
+
+    parser = argparse.ArgumentParser(description=helper)
     parser.add_argument("PATH", help="path to the directory which will be processed")
-    parser.add_argument("-w", help="warning threshold in kB", type=int)
-    parser.add_argument("-c", help="critical threshold in kB", type=int)
+    parser.add_argument("-w", help="warning threshold in kB", type=int, required=True)
+    parser.add_argument("-c", help="critical threshold in kB", type=int, required=True)
     args = parser.parse_args()
-    error_text = "Didn't match the threshold"
-    info_text = "Everything's OK"
+
     shell_output = subprocess.check_output(["du", "-s", args.PATH])
     space = int(shell_output.split()[0])
-    logging.basicConfig(format='%(asctime)s\n\t%(process)d-%(name)s:%(levelname)s:%(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
-
-    # if args.w:
-    #	print(args.w)
-    # if args.c:
-    #	print(args.c)
+    logging.basicConfig(format='%(asctime)s\t%(process)d-%(name)s-%(levelname)s-%(message)s', level=logging.INFO, datefmt='%Y-%m-%d %H:%M:%S')
     if space < args.w:
         logging.info(info_text)
-        return 0, int(time.time()), space
+        status = 0
     elif args.w <= space < args.c:
         logging.warning(error_text)
-        return 1, int(time.time()), space
-    logging.critical(error_text)
-    return 2, int(time.time()), space
+        status = 1
+    else:
+        logging.critical(error_text)
+        status = 2
+    return status, int(time.time()), space
 
 
 if __name__ == "__main__":
