48ab87fd58d90dc88a1adf5e618fa21a8c0f64af
st-rom <r.stepaniuk@ucu.edu.ua>
2019-02-18 16:59:58 +0200

    update

--- src/python/monitors/processes_threshold.py
+++ src/python/monitors/processes_threshold.py
@@ -30,29 +30,6 @@ def main():
     return 0, time.time(), num_of_processes
 
 
-
-
-def bred():
-    str = 'ps -aux' + '|' + "awk '{print $11}'"
-    output = subprocess.check_output([str], shell=True)
-    output = output.decode().split('\n')
-    count = 0
-    for i in range(len(output)):
-        if(output[i]==sys.argv[1]):
-            count += 1
-
-    FORMAT = '%(message)s'
-    logging.basicConfig(format=FORMAT)
-    logger = logging.getLogger('check_output')
-
-    if(count <= int(sys.argv[2]) and count != 0):
-        logger.warning('System status: %s', 'OK')
-    if(count > int(sys.argv[2])):
-        logger.warning('System status: %s', 'CRITICAL')
-    if(count == 0):
-        logger.warning('System status: %s', 'WARNING: NO SUCH PROCESSES RUNNING')
-
-
 if __name__ == "__main__":
     print(main())
     #print(datetime.fromtimestamp(main()[1]))
