e90b510a23e7c2ee1da159fab6f3993ed4f35982
Myroslava <romaniuk@ucu.edu.ua>
2019-02-18 13:56:56 +0200

    logging update

--- processes_threshold.py
+++ processes_threshold.py
@@ -1,16 +1,22 @@
 import subprocess
 import sys
+import logging
 
-str = sys.argv[1] + ' ' + sys.argv[2] + '|' + "awk '{print $11}'"
+str = 'ps -aux' + '|' + "awk '{print $11}'"
 output = subprocess.check_output([str], shell=True)
 output = output.decode().split('\n')
 count = 0
 for i in range(len(output)):
-	if(output[i]==sys.argv[3]):
+	if(output[i]==sys.argv[1]):
 		count += 1
-if(count <= int(sys.argv[4]) and count != 0):
-	print("OK")
-if(count > int(sys.argv[4])):
-	print("CRITICAL")
+
+FORMAT = '%(message)s'
+logging.basicConfig(format=FORMAT)
+logger = logging.getLogger('check_output')
+
+if(count <= int(sys.argv[2]) and count != 0):
+	logger.warning('System status: %s', 'OK')
+if(count > int(sys.argv[2])):
+	logger.warning('System status: %s', 'CRITICAL')
 if(count == 0):
-	print("WARNING: NO SUCH PROCESSES RUNNING")
+	logger.warning('System status: %s', 'WARNING: NO SUCH PROCESSES RUNNING')
