385565457ca3b85bc042a5b9547ce08ee89e9328
st-rom <r.stepaniuk@ucu.edu.ua>
2019-02-21 22:46:37 +0200

    finished tasks

---
+++ src/python/monitors/run_disk_filesystem_status.sh
@@ -0,0 +1,13 @@
+#! /usr/bin/env bash
+
+PRODROOT=${PRODROOT:-/home/roman/PycharmProjects/SoftServe/internship-project}
+PYTHON=${PYTHON:-~/miniconda3/bin/python}
+
+PYSRCROOT=${PYSRCROOT:-${PRODROOT}/src/python}
+PYTHONPATH=${PYTHONPATH:-${PYSRCROOT}}
+CONFROOT=${CONFROOT:-${PRODROOT}/etc}
+PYTHON_EGG_CACHE=${PYTHON_EGG_CACHE:-/tmp/.python-eggs}
+
+export PRODROOT PYSRCROOT PYTHONPATH CONFROOT PYTHON_EGG_CACHE PYTHON
+
+${PYTHON} ${PYSRCROOT}/monitors/disk_filesystem_status.py $*
